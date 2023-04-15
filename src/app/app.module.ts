8dfa4554283c2f603032991694ed3d55e38f2582
Abhinav Kalra <99359469+abhi8080@users.noreply.github.com>
2023-04-15 14:16:53 +0200

    feat: add imports and declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,33 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-const appRoutes: Routes = [{ path: '', component: LoginComponent }];
+import { HttpClientModule } from '@angular/common/http';
+import { MessagesComponent } from './components/messages/messages.component';
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './components/register/register.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MessagesComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MessagesComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
