b824765254b17379166d83c386442800a391a8bf
Abhinav Kalra <99359469+abhi8080@users.noreply.github.com>
2023-04-15 10:40:23 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+
+const appRoutes: Routes = [{ path: '', component: LoginComponent }];
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
