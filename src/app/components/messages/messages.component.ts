bb5fa6d6c50b6ab334432ce50be76c219bedd4f2
Abhinav Kalra <99359469+abhi8080@users.noreply.github.com>
2023-05-01 18:13:09 +0200

    feat: add styling to the login and messages component

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -11,7 +11,12 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class MessagesComponent implements OnInit {
   messages: any;
   message = new Message();
-  @ViewChild('content') private content: any;
+  passcode: string = '';
+  @ViewChild('messageModal') private addMessageModal: any;
+  @ViewChild('joinModal') private joinModal: any;
+  membershipMessage: any = null;
+  isMember: boolean = false;
+  isAdmin: boolean = false;
 
   constructor(
     private dataService: DataService,
@@ -20,21 +25,48 @@ export class MessagesComponent implements OnInit {
 
   ngOnInit(): void {
     this.getMessages();
+    this.getUserAdminStatus();
   }
 
-  getMessages() {
-    this.dataService.getMessages().subscribe((res) => {
-      this.messages = res;
-    });
+  async getMessages() {
+    const response = await this.dataService.getMessages();
+    this.messages = await response.data;
+    if (this.messages[0].user_id !== 'Anonymous') this.isMember = true;
   }
 
-  openModal() {
-    this.modalService.open(this.content);
+  addMessage() {
+    this.modalService.open(this.addMessageModal);
+  }
+
+  handleJoinTheClub() {
+    this.modalService.open(this.joinModal);
   }
 
   createMessage() {
     this.dataService.insertMessage(this.message).subscribe(() => {
       this.getMessages();
     });
+  }
+
+  async removeMessage(messageId: number) {
+    try {
+      await this.dataService.removeMessage(messageId);
+      this.getMessages();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async sumbitPasscode() {
+    if (this.passcode.toUpperCase() === 'TOKYO') {
+      await this.dataService.updateMembershipStatus();
+      this.membershipMessage = 'You are a member now!';
+      this.getMessages();
+    } else this.membershipMessage = 'The passcode is not correct. Try again!';
+  }
+
+  async getUserAdminStatus() {
+    const response = await this.dataService.getUserAdminStatus();
+    if ((await response.data.isAdmin) == 1) this.isAdmin = true;
   }
 }
